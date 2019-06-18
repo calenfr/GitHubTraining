61a780f8583e9d09910f6d4f39ee89bd690bb66b
calenfr <51925673+calenfr@users.noreply.github.com>
2019-06-18 10:52:34 -0700

    Add files via upload
    Trial 2: This is the update with the average. I'm not sure if the code was uploaded or not.

--- readnc.py
+++ readnc.py
@@ -2,34 +2,27 @@
 
 import netCDF4
 import numpy as np
+from netCDF4 import Dataset  # http://code.google.com/p/netcdf4-python/   #Dataset is the type
 
-from netCDF4 import Dataset  # http://code.google.com/p/netcdf4-python/
-
-#Dataset is the type
-
+#Coordinate Points (~Pendleton~)
 y=60
 x=90
+
+ticker=0
+datanew=0
+
 filein = netCDF4.Dataset('gfdl-cm3.2098090100.nc')         #Reads in specific file gfdl-cm3.2098090100.nc
-mint = filein.variables['T2MIN']                               #Stores individual variable
-data = mint[:,y,x]
-
-for t in range(0,mint.shape[0]) :
-    print([data[t]])
-
-#print(mint.shape)
+mint = filein.variables['T2MIN']                           #Stores individual variable
+data = mint[:,y,x]                                         #Files into a month long array
+#print(data)                                               #Use to check data points
+#print(mint.shape)                                         #Use to check array size
 #print(mint.dimensions)
 
+#for t in range(0,4) :                                     #Use for a smaller range than entire month
 
-#ticker = 0
-#datenew = 0
-#
-#Loop -> Data Point Exists
-#for ()
-#ticker=ticker+1
-#datanew=data+datanew
-#Exit Loop
+for t in range(0,mint.shape[0]) :
+    datanew=data[ticker]+datanew
+    ticker=ticker+1                      
 
-#average=(datanew)/ticker
-#print(average)
-
-
+average=datanew/ticker
+print(average)
